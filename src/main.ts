19580224b37bc0581b6f7a9b58561c4326da58b5
zjx <2790627871@qq.com>
2023-05-15 17:20:16 +0800

    feat(global): 配置页面路由

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(store)
+app.use(router)
+app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: () =>
+      import(/* webpackChunkName: "login" */ '../views/login.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/main',
+    name: 'main',
+    component: () => import(/* webpackChunkName: "main" */ '../views/main.vue')
   }
 ]
 
